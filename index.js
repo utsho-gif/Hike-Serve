716eb95c7e795e9000b0e7e6bd75e1bc74b95389
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-04-30 04:45:46 +0600

    connected with database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,6 +14,33 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Home');
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8xmqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+       await client.connect();
+        const productCollection = client.db("hikeServe").collection("product");
+        const userCollection = client.db('hikeServe').collection('user');
+
+
+       //load data
+       app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+       })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.listen(port, () => {
